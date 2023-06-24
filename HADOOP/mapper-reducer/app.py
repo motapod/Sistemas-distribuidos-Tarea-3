f619ee7ab9aca17a7ff12a877c635d1598bba70c
motapod <65835408+motapod@users.noreply.github.com>
2023-06-24 13:44:51 -0400

    se debe agregar titles y names

--- HADOOP/mapper-reducer/app.py
+++ HADOOP/mapper-reducer/app.py
@@ -14,15 +14,15 @@ for line in f2:
     urls.append(line)
 
 # Input the word to search
-word = input("Que palabra buscas? ")
+word = input("Que actor buscas? ")
 
 # Search the word in the json database and see wich file contains the word more times
 if word in data:
-    print("La palabra " + word + " se encuentra en los siguientes archivos:")
+    print("El actor " + word + " se encuentra en las siguientes peliculas:")
     for i in data[word]:
         print(urls[i])
     # Print the number of times the word appears in each url
-    print("La palabra " + word + " aparece " + str(data[word][0]) + " veces en el archivo " + urls[data[word][1]])
+    print("El actor " + word + " aparece " + str(data[word][0]) + " veces en el archivo " + urls[data[word][1]])
 else:
-    print("La palabra " + word + " no se encuentra en los archivos")
+    print("El actor " + word + " no se encuentra en las peliculas")
     
