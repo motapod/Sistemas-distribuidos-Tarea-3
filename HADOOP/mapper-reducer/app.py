8f572071530f6bc55cd06dfbe87ae92d6e8c5558
motapod <65835408+motapod@users.noreply.github.com>
2023-06-26 18:49:51 -0400

    a

--- HADOOP/mapper-reducer/app.py
+++ HADOOP/mapper-reducer/app.py
@@ -1,17 +1,54 @@
 import json
+import os
+
+import pandas as pd
+
+actor_movies = {}
+
+reader=  pd.read_csv('title_movie.tsv', sep='\t')
+#next(reader)   Omitir la primera línea de encabezado
+for i, row in enumerate(reader):
+            if i >= 5:
+                break
+            print(row)
+#if not reader.__sizeof__ == 0:
+#    for row in reader:
+#        print(row)
+
+for row in reader:
+    actor_id = row[0]
+    movie_title = row[2]
+
+    if actor_id not in actor_movies:
+        actor_movies[actor_id] = []
+
+    actor_movies[actor_id].append(movie_title)
+actor_names = {}
+
+j =   open("./name_actor.tsv", "r")
+reader = csv.reader(j, delimiter='\t')
+if not reader.__sizeof__ == 0:
+        for row in reader:
+            print(row)
+
+
+for row in reader:
+    actor_id = row[0]
+    actor_name = row[1]
+    actor_names[actor_id] = actor_name
+
+with open('resultado.tsv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file, delimiter='\t')
+    writer.writerow(['Actor', 'Películas'])
+
+    for actor_id, actor_name in actor_names.items():
+        movies = actor_movies.get(actor_id, [])
+        writer.writerow([actor_name, ', '.join(movies)])
+
 
 # Create a json database
-f1 = open('data.json')
-data = json.load(f1)
-
-# Save urls in a array 
-urls = []
-urls.append(0)
-
-# Save the content of the URL file in urls arrays 
-f2 = open("URL.txt", "r")
-for line in f2:
-    urls.append(line)
+with open(os.path.expanduser('HADOOP\mapper-reducer\data.json'), 'a+') as f:
+    data = json.load(f)
 
 # Input the word to search
 word = input("Que actor buscas? ")
