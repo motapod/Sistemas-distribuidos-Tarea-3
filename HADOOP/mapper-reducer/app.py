73494c598f8d7c978e707dd8eada3372f73eaa8a
marymarito <82898754+marymarito@users.noreply.github.com>
2023-06-27 17:21:20 -0400

    vamo xile

--- HADOOP/mapper-reducer/app.py
+++ HADOOP/mapper-reducer/app.py
@@ -1,20 +1,18 @@
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+import csv
 import json
 import os
 
 import pandas as pd
 
 actor_movies = {}
-
-reader=  pd.read_csv('title_movie.tsv', sep='\t')
-#next(reader)   Omitir la primera línea de encabezado
+# Tengo que leer el archivo tsv separandolo por tabulaciones
+reader = pd.read_csv('title_movie.tsv', sep='\t')
+# next(reader)   Omitir la primera línea de encabezado
 for i, row in enumerate(reader):
-            if i >= 5:
-                break
-            print(row)
-#if not reader.__sizeof__ == 0:
-#    for row in reader:
-#        print(row)
-
+    if i >= 5:
+        break
+    print(row)
 for row in reader:
     actor_id = row[0]
     movie_title = row[2]
@@ -25,17 +23,13 @@ for row in reader:
     actor_movies[actor_id].append(movie_title)
 actor_names = {}
 
-j =   open("./name_actor.tsv", "r")
-reader = csv.reader(j, delimiter='\t')
-if not reader.__sizeof__ == 0:
-        for row in reader:
-            print(row)
+with open("./name_actor.tsv", "r", encoding='utf-8') as j:
+    reader = csv.reader(j, delimiter='\t')
 
-
-for row in reader:
-    actor_id = row[0]
-    actor_name = row[1]
-    actor_names[actor_id] = actor_name
+    for row in reader:
+        actor_id = row[0]
+        actor_name = row[1]
+        actor_names[actor_id] = actor_name
 
 with open('resultado.tsv', 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file, delimiter='\t')
@@ -47,19 +41,19 @@ with open('resultado.tsv', 'w', encoding='utf-8', newline='') as file:
 
 
 # Create a json database
-with open(os.path.expanduser('HADOOP\mapper-reducer\data.json'), 'a+') as f:
+with open('data.json', 'a+') as f:
     data = json.load(f)
 
 # Input the word to search
-word = input("Que actor buscas? ")
+#word = input("Que actor buscas? ")
 
 # Search the word in the json database and see wich file contains the word more times
-if word in data:
-    print("El actor " + word + " se encuentra en las siguientes peliculas:")
-    for i in data[word]:
-        print(urls[i])
+#if word in data:
+ #   print("El actor " + word + " se encuentra en las siguientes peliculas:")
+  #  for i in data[word]:
+   #     print(urlsafe_b64decode[i])
     # Print the number of times the word appears in each url
-    print("El actor " + word + " aparece " + str(data[word][0]) + " veces en el archivo " + urls[data[word][1]])
-else:
-    print("El actor " + word + " no se encuentra en las peliculas")
+    #print("El actor " + word + " aparece " + str(data[word][0]) + " veces en el archivo " + urlsafe_b64encode[data[word][1]])
+#else:
+ #   print("El actor " + word + " no se encuentra en las peliculas")
     
--- HADOOP/mapper-reducer/mapper.py
+++ HADOOP/mapper-reducer/mapper.py
@@ -1,20 +1,38 @@
-import sys
-import re
-import os
+from mrjob.job import MRJob
+import csv
 
-# Create a list with the words of the file
-for line in sys.stdin:
-    # Create a list with the words of the file
-    line = re.sub(r'\W+',' ',line.strip())
-    words = line.split()
+class MovieActorIndex(MRJob):
     
+    def configure_args(self):
+        super(MovieActorIndex, self).configure_args()
+        self.add_file_arg('--actors', help='name_actor.tsv')
+        self.add_file_arg('--movies', help='title_movie.tsv')
+        
+    def mapper_init(self):
+        # Cargar el archivo name_actor.tsv
+        print("File path:", self.options.movies)  # Agrega esta línea para imprimir la ruta del archivo
+        with open(self.options.movies, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f, delimiter='\t')
+            self.actor_names = {row[0]: row[1] for row in reader}
+        
+    def mapper(self, _, line):
+        print(line)
+        # Emitir pares clave-valor para actores y películas
+        if 'titleType' not in line:
+            row = line.split('\t')
+            actor_id = row[0]
+            movie_id = row[1]
+            if actor_id in self.actor_names:
+                actor_name = self.actor_names[actor_id]
+                yield actor_name, movie_id
+                
+    def reducer(self, actor_name, movie_ids):
+        # Generar la lista de películas para cada actor
+        movie_list = list(movie_ids)
+        yield actor_name, movie_list
 
-    root = os.environ['map_input_file']
-    root_list = root.split("/", 6)
-    documento = root_list[6].split('.', 1)[0]
-    documento = documento.split('o', 1)[1]
-
-    # Iterate over the list of words
-    for word in words:
-        # Write the result to STDOUT
-        print('{}\t{}\t{}'.format(word, 1, int(documento)))
+if __name__ == '__main__':
+    MovieActorIndex.run()
+## para instalar el mapper, se debe instalar mrjob. Y eso lo haces en terminal
+## con el comando: pip install mrjob
+## y luego para correrlo le doy el comando: python mapper.py --actors name_actor.tsv --movies title_movie.tsv > resultado.tsv
